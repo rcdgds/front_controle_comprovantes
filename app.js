f09d1ba6d1d865f0b5dc0b361fdc68959b7ae411
Ricardo Gomes <rcdgds@gmail.com>
2026-08-06 10:36:45 +0100

    add captura de numero de parcelas formulario

--- app.js
+++ app.js
@@ -1,44 +1,46 @@
-// 1. Inicializa o SDK do Telegram
-const tg = window.Telegram.WebApp;
+    // 1. Inicializa o SDK do Telegram
+    const tg = window.Telegram.WebApp;
 
-// Configurações iniciais da janela do Telegram
-tg.expand();
-tg.ready();
+    // Configurações iniciais da janela do Telegram
+    tg.expand();
+    tg.ready();
 
-// Ajuste dinâmico de tema (Claro/Escuro)
-if (tg.colorScheme === 'light') {
-    document.body.classList.remove('bg-gray-900', 'text-gray-100');
-    document.body.classList.add('bg-white', 'text-gray-800');
-}
-
-// 2. Elementos do DOM
-const form = document.getElementById('contaForm');
-
-// 3. Listener do Evento de Submit
-form.addEventListener('submit', function(event) {
-    event.preventDefault(); // Impede o reload da página
-
-    // Captura os valores digitados
-    const descricao = document.getElementById('descricao').value.trim();
-    const valorTotal = document.getElementById('valor').value;
-    const classificacaoDespesa = document.getElementById('classificacaoDespesa').value;
-    const dataVencimento = document.getElementById('dataVencimento').value;
-
-    // Validação
-    if (!descricao || isNaN(valorTotal) || !classificacaoDespesa || !dataVencimento) {
-        tg.showAlert("Por favor, preencha todos os campos corretamente.");
-        return;
+    // Ajuste dinâmico de tema (Claro/Escuro)
+    if (tg.colorScheme === 'light') {
+        document.body.classList.remove('bg-gray-900', 'text-gray-100');
+        document.body.classList.add('bg-white', 'text-gray-800');
     }
 
-    // Objeto DTO que espelha seu Record em Java
-    const payload = {
-        descricao: descricao,
-        valorTotal: valorTotal,
-        classificacaoDespesa: classificacaoDespesa,
-        dataVencimento: dataVencimento
-    };
+    // 2. Elementos do DOM
+    const form = document.getElementById('contaForm');
 
-    
-    // Envia a String JSON para o Telegram Webhook
-    tg.sendData(JSON.stringify(payload));
-});
+    // 3. Listener do Evento de Submit
+    form.addEventListener('submit', function(event) {
+        event.preventDefault(); // Impede o reload da página
+
+        // Captura os valores digitados
+        const descricao = document.getElementById('descricao').value.trim();
+        const valorTotal = document.getElementById('valor').value;
+        const numeroDeParcelas = document.getElementById('numeroDeParcelas').value;
+        const classificacaoDespesa = document.getElementById('classificacaoDespesa').value;
+        const dataVencimento = document.getElementById('dataVencimento').value;
+
+        // Validação
+        if (!descricao || isNaN(valorTotal) || isNaN(valorTotal) || !classificacaoDespesa || !dataVencimento) {
+            tg.showAlert("Por favor, preencha todos os campos corretamente.");
+            return;
+        }
+
+        // Objeto DTO que espelha seu Record em Java
+        const payload = {
+            descricao: descricao,
+            valorTotal: valorTotal,
+            numeroDeParcelas : numeroDeParcelas,
+            classificacaoDespesa: classificacaoDespesa,
+            dataVencimento: dataVencimento
+        };
+
+        
+        // Envia a String JSON para o Telegram Webhook
+        tg.sendData(JSON.stringify(payload));
+    });
